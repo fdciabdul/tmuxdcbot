ebd3ba314132c9d3ea8b15842a1da7819b67d34c
Viki991-DBot <65564072+Viki991-DBot@users.noreply.github.com>
2021-03-29 23:52:39 +0700

    Update botLogin.js

--- config/botLogin.js
+++ config/botLogin.js
@@ -1,5 +1,5 @@
 module.exports = {
-	token: "TOKEN_KEY",
-	youtubeAPI: "YOUTUBE_API_KEY",
-	owner_id: "OWNER_ID"
+	token: "NzU2MTY0MjIxOTcyMjUwNzM0.X2N2zw.tJtDA6YT0qjGjS2cUA6EVrq5QkQ",
+	youtubeAPI: "AIzaSyAvG8TIPsYfedTJBBmGK5TwH0BNEcej7MI",
+	owner_id: "666947160310546432"
 }
