ba998897898a8762b46c1f975a564c4191de8e1d
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2021-05-20 16:27:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-const config = require("config");
+const config = require("./config");
 const banner = require("ascii-art");
 require("dotenv").config();
 var clc = require("cli-color");
