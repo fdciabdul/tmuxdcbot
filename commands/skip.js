393db845842bec2e8cf65b0380adff3f9d086912
Abdul ムッタキン <abdulmuttaqin456@gmail.com>
2022-02-06 03:04:20 +0700

    Delete commands directory

--- commands/skip.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "skip",
-  aliases: ["s"],
-  async run(message, args, client) {
-    let queue = client.queues.get(message.guild.id);
-    if (!queue) return message.channel.send("No music playing currently!");
-    queue.dispatcher.end();
-  },
-};
